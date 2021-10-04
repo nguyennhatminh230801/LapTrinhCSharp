4e3efdef8566f991d879bd524201b99944cb0311
= <=>
2021-10-04 21:49:19 +0700

    bt truoc khi len lop bai 5

---
+++ Tuan 5/BT Truoc Khi Len Lop Tuan 5/NguyenNhatMinh_2019600285_proj51/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NguyenNhatMinh_2019600285_proj51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            List<ThiSinhA> danhSachThiSinhA = new List<ThiSinhA>();
+            int choose = 7;
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("=====================================================");
+                    Console.WriteLine("===================QUẢN LÝ THÍ SINH==================");
+                    Console.WriteLine("=====================================================");
+                    Console.WriteLine("=1. Nhập Thông Tin Sinh Viên                        =");
+                    Console.WriteLine("=2. Hiển Thị Danh Sách Thí Sinh                     =");
+                    Console.WriteLine("=3. Hiển Thị Các Sinh Viên Theo Tổng Điểm           =");
+                    Console.WriteLine("=4. Hiển Thị Sinh Viên Theo Địa Chỉ                 =");
+                    Console.WriteLine("=5. Tìm Kiếm Theo Số Báo Danh                       =");
+                    Console.WriteLine("=6. Thoát Chương Trình                              =");
+                    Console.WriteLine("=====================================================");
+                    Console.Write("Nhập lựa chọn: ");
+                    choose = int.Parse(Console.ReadLine());
+                    Console.WriteLine("=====================================================");
+
+                    switch(choose)
+                    {
+                        case 1:
+                            Console.Clear();
+                            NhapThongTinSinhVien(ref danhSachThiSinhA);
+                            break;
+                        case 2:
+                            Console.Clear();
+                            HienThiDanhSachSinhVien(danhSachThiSinhA);
+                            break;
+                        case 3:
+                            Console.Clear();
+                            Console.Write("Nhập tổng điểm: ");
+                            double TongDiem = double.Parse(Console.ReadLine());
+                            HienThiDanhSachSinhVienTheoTongDiem(danhSachThiSinhA, TongDiem);
+                            break;
+                        case 4:
+                            Console.Clear();
+                            Console.Write("Nhập địa chỉ: ");
+                            string diaChi = Console.ReadLine().Trim();
+                            HienThiDanhSachSinhVienTheoDiaChi(danhSachThiSinhA, diaChi);
+                            break;
+                        case 5:
+                            Console.Clear();
+                            Console.Write("Nhập số báo danh: ");
+                            string soBaoDanh = Console.ReadLine().Trim();
+                            TimKienSoBaoDanh(danhSachThiSinhA, soBaoDanh);
+                            break;
+                        case 6:
+                            Console.Clear();
+                            Console.WriteLine("Bạn muốn thoát chương trình?");
+                            Console.WriteLine("1. Có");
+                            Console.WriteLine("2. Không");
+                            int choose3 = int.Parse(Console.ReadLine());
+
+                            if(choose3 == 2)
+                            {
+                                choose = 7;
+                            }
+
+                            break;
+
+                        default:
+                            break;
+                    }
+                    
+                    if(choose == 6)
+                    {
+                        break;
+                    }
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("Bạn không được nhập sai kiểu số!!!");
+                    continue;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    continue;
+                }
+            }
+        }
+
+        public static void TimKienSoBaoDanh(List<ThiSinhA> danhSachThiSinhA, string soBaoDanh)
+        {
+            if (danhSachThiSinhA.Count == 0)
+            {
+                Console.WriteLine("Danh sách này rỗng".ToUpper());
+                return;
+            }
+
+            ThiSinhA thiSinhA= danhSachThiSinhA.Find(element => element.SoBaoDanh.Trim().ToLower() == soBaoDanh.Trim().ToLower());
+
+            if(thiSinhA == null)
+            {
+                Console.WriteLine("Không tìm được thí sinh này".ToUpper());
+                return;
+            }
+
+            Console.WriteLine("Thông tin thí sinh: ".ToUpper());
+            Console.WriteLine(thiSinhA);
+        }
+
+        public static void HienThiDanhSachSinhVienTheoDiaChi(List<ThiSinhA> danhSachThiSinhA, string diaChi)
+        {
+            if (danhSachThiSinhA.Count == 0)
+            {
+                Console.WriteLine("Danh sách này rỗng".ToUpper());
+                return;
+            }
+
+            var query = (from thiSinhA in danhSachThiSinhA
+                         where thiSinhA.DiaChi.Contains(diaChi)
+                         select thiSinhA).ToList();
+
+            if (query.Count == 0)
+            {
+                Console.WriteLine("Không có thí sinh nào ở nơi này".ToUpper());
+                return;
+            }
+
+            HienThiDanhSachSinhVien(query);
+        }
+
+        public static void HienThiDanhSachSinhVienTheoTongDiem(List<ThiSinhA> danhSachThiSinhA, double tongDiem)
+        {
+            if (danhSachThiSinhA.Count == 0)
+            {
+                Console.WriteLine("Danh sách này rỗng".ToUpper());
+                return;
+            }
+
+            var query = (from thiSinhA in danhSachThiSinhA
+                        where thiSinhA.TongDiem >= tongDiem
+                        select thiSinhA).ToList();
+            
+            if(query.Count == 0)
+            {
+                Console.WriteLine("Không có thí sinh nào vượt tổng điểm này".ToUpper());
+                return;
+            }
+
+            HienThiDanhSachSinhVien(query);
+        }
+
+        public static void HienThiDanhSachSinhVien(List<ThiSinhA> danhSachThiSinhA)
+        {
+            if(danhSachThiSinhA.Count == 0)
+            {
+                Console.WriteLine("Danh sách này rỗng".ToUpper());
+                return;
+            }
+
+            int i = 1;
+            Console.WriteLine("danh sách thí sinh:".ToUpper());
+            foreach (var item in danhSachThiSinhA)
+            {
+                Console.WriteLine($"Sinh viên thứ {i++}: ");
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void NhapThongTinSinhVien(ref List<ThiSinhA> danhSachThiSinhA)
+        {
+            ThiSinhA thiSinhA = new ThiSinhA();
+            Console.WriteLine("Nhập mới một thí sinh: ".ToUpper());
+            thiSinhA.Nhap();
+
+            int choose2 = 0;
+            Console.WriteLine("\nBạn có lưu hay không ?");
+            Console.WriteLine("1. Có");
+            Console.WriteLine("2. Không");
+            choose2 = int.Parse(Console.ReadLine());
+
+            if(choose2 == 2)
+            {
+                return;
+            }
+
+            danhSachThiSinhA.Add(thiSinhA);
+        }
+    }
+}
---
+++ Tuan 5/BT Truoc Khi Len Lop Tuan 5/NguyenNhatMinh_2019600285_proj51/ThiSinhA.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace NguyenNhatMinh_2019600285_proj51
+{
+    class ThiSinhA
+    {
+        public string SoBaoDanh { get; set; }
+        public string HoTen { get; set; }
+        public string DiaChi { get; set; }
+        public double DiemToan { get; set; }
+        public double DiemLy { get; set; }
+        public double DiemHoa { get; set; }
+        public double DiemUuTien { get; set; }
+
+        private double tongDiem;
+        public double TongDiem { get => tongDiem; set => tongDiem = value; }
+
+        public ThiSinhA()
+        {
+            SoBaoDanh = "";
+            HoTen = "";
+            DiaChi = "";
+            DiemToan = 0;
+            DiemLy = 0;
+            DiemHoa = 0;
+            DiemUuTien = 0;
+            TongDiem = 0;
+        }
+
+        public ThiSinhA(string soBaoDanh)
+        {
+            SoBaoDanh = soBaoDanh;
+            HoTen = "";
+            DiaChi = "";
+            DiemToan = 0;
+            DiemLy = 0;
+            DiemHoa = 0;
+            DiemUuTien = 0;
+            TongDiem = 0;
+        }
+
+        public ThiSinhA(string soBaoDanh, string hoTen, string diaChi, double diemToan, double diemLy, double diemHoa, double diemUuTien)
+        {
+            SoBaoDanh = soBaoDanh;
+            HoTen = hoTen;
+            DiaChi = diaChi;
+            DiemToan = diemToan;
+            DiemLy = diemLy;
+            DiemHoa = diemHoa;
+            DiemUuTien = diemUuTien;
+            TongDiem = DiemHoa + DiemToan + DiemLy + DiemUuTien;
+        }
+
+        public void Nhap()
+        {
+            Console.Write("Nhập Số Báo Danh: ");
+            SoBaoDanh = Console.ReadLine().Trim();
+            Console.Write("Nhập Họ Tên: ");
+            HoTen = Console.ReadLine().Trim();
+            Console.Write("Nhập Địa Chỉ: ");
+            DiaChi = Console.ReadLine().Trim();
+            Console.Write("Nhập Điểm Toán: ");
+            DiemToan = double.Parse(Console.ReadLine());
+            Console.Write("Nhập Điểm Lý: ");
+            DiemLy = double.Parse(Console.ReadLine());
+            Console.Write("Nhập Điểm Hóa: ");
+            DiemHoa = double.Parse(Console.ReadLine());
+            Console.Write("Nhập Điểm Uư Tiên: ");
+            DiemUuTien = double.Parse(Console.ReadLine());
+
+            TongDiem = DiemHoa + DiemToan + DiemLy + DiemUuTien;
+        }
+
+        //Nạp chồng xuất đối tượng
+        public override string ToString()
+        {
+            return $"Số báo danh: {SoBaoDanh}" +
+                $"\nHọ tên: {HoTen}" +
+                $"\nĐịa Chỉ: {DiaChi}" +
+                $"\nĐiểm Toán: {DiemToan}" +
+                $"\nĐiểm Lý: {DiemLy}" +
+                $"\nĐiểm Hóa: {DiemHoa}" +
+                $"\nĐiêm Ưu Tiên: {DiemUuTien}" +
+                $"\nTổng Điểm: {TongDiem}";
+        }
+    }
+}
